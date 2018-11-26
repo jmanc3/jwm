579ee5d264ab7b77985339a4f31281ef18a8384d
Jerry Gongora <jerrygongora@gmail.com>
2018-11-25 22:05:18 -0600

    terminate xinit shortcut

--- config.h
+++ config.h
@@ -30,7 +30,6 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       3,       0,           -1 },
 };
 
 /* layout(s) */
@@ -60,6 +59,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+
+static const char *fullquit[] = {"killall", "xinit"};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -96,6 +97,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+    {MODKEY,                        XK_q,      spawn,          {.v = fullquit}},
 };
 
 /* button definitions */
